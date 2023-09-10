5e9a782c9335c56f089614ff74cab54161f74a9b
ivorszaniszloRingier <szaniszlo.ivor@ringier.hu>
2023-09-10 16:03:14 +0200

    ADD: form

--- src/components/Meals/MelItem/MealItem.js
+++ src/components/Meals/MelItem/MealItem.js
@@ -1,4 +1,6 @@
+import MealItemForm from './MealItemForm';
 import classes from './MealItem.module.css';
+
 
 const MealItem = (props) => {
     const price = `$${props.price.toFixed(2)}`;
@@ -10,7 +12,7 @@ const MealItem = (props) => {
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-
+        <MealItemForm />
       </div>
     </li>
   );
